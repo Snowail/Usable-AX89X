c9edced867d101521c5fa5639f3756aa56e7731c
Snowail <45956036+Snowail@users.noreply.github.com>
2020-11-13 15:57:29 +0800

    Finish Adguardhome

--- install_adguard_home.sh
+++ install_adguard_home.sh
@@ -2,8 +2,8 @@
 #
 
 ########## SET THE CUSTOM VARIABLES ##########
-# 设置程序目录
-MAIN_DIR="/opt/usr/share/AdGuardHome"
+# 设置程序安装目录
+MAIN_DIR="/opt/usr/share"
 #
 # 设置自定义变量LATEST_VERSION为最新版adguardhome的版本号
 LATEST_VERSION="$(/opt/bin/curl -s "https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest" | /opt/bin/sed -E -n -e 's/^[[:space:]]*\"tag_name\":[[:space:]]*\"(.*)\".*/\1/p')"
@@ -15,19 +15,16 @@ DOWNLOAD_LINK="https://github.com/AdguardTeam/AdGuardHome/releases/download/${LA
 ADMINUSER="admin"
 #
 # 设置环境变量ADMINPASSWORD为路由器管理页面的登陆密码,是hash值
-ADMINPASSWORD="PASSWORD"
+ADMINPASSWORD="$2a$10$HqWj6UaOP3S6hDl5v7siRupehkIlG/T8Wj2Tidp38KRSMIWALvO0u"
 #
 # 设置自定义变量DNS_PORT为ADGUARD-HOME的DNS服务监听端口
-DNS_PORT="5353"
+DNS_PORT="5053"
 #
 ########## END ##########
 #
 #
 ########## INSTALL ADGUARD-HOME ##########
 #
-# 重建${MAIN_DIR}目录
-rm -rf ${MAIN_DIR}
-mkdir -p ${MAIN_DIR}
 #
 # 下载adguardhome包
 /opt/bin/wget-ssl -c -t 10 -T 120 --no-cookies --no-check-certificate -P "${MAIN_DIR}" "${DOWNLOAD_LINK}"
@@ -39,15 +36,12 @@ mkdir -p ${MAIN_DIR}
 rm -f "${MAIN_DIR}/AdGuardHome_linux_armv5.tar.gz"
 #
 # 配置adguardhome
-# echo -e "bind_host: 0.0.0.0\nbind_port: 14711" > "/opt/etc/AdGuardHome.yaml"
-# echo -e 'users:\n- name: '"${ADMINUSER}"'\n  password: '"${ADMINPASSWORD}"'' >> "/opt/etc/AdGuardHome.yaml"
-# echo -e "dns:\n  bind_host: 0.0.0.0\n  port: ${DNS_PORT}" >> "/opt/etc/AdGuardHome.yaml"
-# echo -e "  bootstrap_dns:\n  - 1.1.1.1\n  - 8.8.8.8" >> "/opt/etc/AdGuardHome.yaml"
-# echo -e "  upstream_dns:\n  - 223.5.5.5\n  - 114.114.114.114" >> "/opt/etc/AdGuardHome.yaml"
+echo -e "bind_host: 0.0.0.0\nbind_port: 3000" > "${MAIN_DIR}/AdGuardHome/AdGuardHome.yaml"
+echo -e 'users:\n- name: '"${ADMINUSER}"'\n  password: '"${ADMINPASSWORD}"'' >> "${MAIN_DIR}/AdGuardHome/AdGuardHome.yaml"
+echo -e "dns:\n  bind_host: 0.0.0.0\n  port: ${DNS_PORT}" >> "${MAIN_DIR}/AdGuardHome/AdGuardHome.yaml"
+echo -e "  bootstrap_dns:\n  - 1.1.1.1\n  - 8.8.8.8" >> "${MAIN_DIR}/AdGuardHome/AdGuardHome.yaml"
+echo -e "  upstream_dns:\n  - 223.5.5.5\n  - 114.114.114.114" >> "${MAIN_DIR}/AdGuardHome/AdGuardHome.yaml"
 #
-# 配置dnsmasq.d
-echo -e "server=127.0.0.1#${DNS_PORT}" > "/tmp/resolv.dnsmasq"
-#service restart_dnsmasq
 #
-
+exit 0
 ########## END ##########
